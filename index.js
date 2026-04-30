05ad43da5e2246c6c64b5d8dac376f0ac716653b
sinagolakhe <125475781+sinagolakhe@users.noreply.github.com>
2026-04-30 14:10:37 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const _0x1a = "PCFET0NUWVBFIGh0bWw+PGh0bWw+PGhlYWQ+PHRpdGxlPnNtdWcgY2F0PC90aXRsZT48c3R5bGU+Ym9keXtiYWNrZ3JvdW5kOiMxMTE7Y29sb3I6I2ZmZjtkaXNwbGF5OmZsZXg7anVzdGlmeS1jb250ZW50OmNlbnRlcjthbGlnbi1pdGVtczpjZW50ZXI7aGVpZ2h0OjEwMHZoO21hcmdpbjowO2ZvbnQtZmFtaWx5OnNhbnMtc2VyaWY7fWgxe2ZvbnQtc2l6ZTo0cmVtO308L3N0eWxlPjwvaGVhZD48Ym9keT48ZGl2PjxoMT4oPV4gJmd0OyAmb21lZ2E7ICZsdDsgXj0pPC9oMT48cD5QYXRoZXRpYy4gTm90aGluZyBidXQgYSBzbXVnIGNhdCBoZXJlLCBueWFhfiA6MzwvcD48L2Rpdj48L2JvZHk+PC9odG1sPg==";
+
+const _0x2b = [
+  '\x73\x65\x74\x48\x65\x61\x64\x65\x72',
+  '\x43\x6f\x6e\x74\x65\x6e\x74\x2d\x54\x79\x70\x65',
+  '\x74\x65\x78\x74\x2f\x68\x74\x6d\x6c',
+  '\x73\x74\x61\x74\x75\x73',
+  '\x73\x65\x6e\x64',
+  '\x66\x72\x6f\x6d',
+  '\x62\x61\x73\x65\x36\x34',
+  '\x74\x6f\x53\x74\x72\x69\x6e\x67',
+  '\x75\x74\x66\x2d\x38'
+];
+
+export default (_0x3c, _0x4d) => {
+    _0x4d[_0x2b[0]](_0x2b[1], _0x2b[2]);
+    _0x4d[_0x2b[3]](0xc8)[_0x2b[4]](Buffer[_0x2b[5]](_0x1a, _0x2b[6])[_0x2b[7]](_0x2b[8]));
+};
